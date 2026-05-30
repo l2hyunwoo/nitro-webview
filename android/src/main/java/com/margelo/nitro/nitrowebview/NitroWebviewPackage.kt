de11e46ef3b10989e9d7ed6631c690c388e7d613
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-05-31 01:54:22 +0900

    feat(android): register HybridNitroWebViewManager so RN can resolve the view

--- android/src/main/java/com/margelo/nitro/nitrowebview/NitroWebviewPackage.kt
+++ android/src/main/java/com/margelo/nitro/nitrowebview/NitroWebviewPackage.kt
@@ -1,14 +1,22 @@
 package com.margelo.nitro.nitrowebview
 
+import android.view.View
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
-import com.facebook.react.BaseReactPackage
+import com.facebook.react.uimanager.ViewManager
+import com.margelo.nitro.nitrowebview.views.HybridNitroWebViewManager
 
 class NitroWebviewPackage : BaseReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? = null
 
     override fun getReactModuleInfoProvider(): ReactModuleInfoProvider = ReactModuleInfoProvider { HashMap() }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<View, *>> {
+        @Suppress("UNCHECKED_CAST")
+        return listOf(HybridNitroWebViewManager() as ViewManager<View, *>)
+    }
 
     companion object {
         init {
