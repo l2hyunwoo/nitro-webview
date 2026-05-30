9c82650ed9eb4af6dea86c55e24ccef08a4a24ea
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-05-31 00:14:46 +0900

    feat(public-api): re-export source, events, bridge, and spec types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
-// TODO: Export all HybridObjects here for the user
+export type { HtmlSource } from './specs/WebViewSource'
+
+export {
+  isHtmlSource,
+  isUriSource,
+  normalizeHtmlSource,
+  sourceToCommand,
+} from './sourceToCommand'
+
+export type {
+  LoadHtmlCommand,
+  LoadUrlCommand,
+  NativeViewCommand,
+} from './nativeCommands'
+
+export { createLoadStartDispatcher } from './events'
+export type {
+  LoadStartDispatcher,
+  NativeLoadStartPayload,
+  OnLoadStart,
+} from './events'
+
+export { createLoadDispatcher } from './events'
+export type {
+  LoadDispatcher,
+  NativeLoadPayload,
+  OnLoad,
+} from './events'
+
+export { createLoadEndDispatcher } from './events'
+export type {
+  LoadEndDispatcher,
+  LoadEndOutcome,
+  NativeLoadEndPayload,
+  OnLoadEnd,
+} from './events'
+
+export type {
+  NitroWebViewErrorEvent,
+  WebViewErrorEvent,
+} from './specs/NitroWebView.nitro'
+
+export {
+  ANDROID_NATIVE_BRIDGE_NAME,
+  BRIDGE_NAME,
+  buildBridgeScript,
+  evaluateBridgeScript,
+} from './bridgeScript'
+export type {
+  AndroidBridgeSandbox,
+  AndroidNativeBridge,
+  BridgePlatform,
+  IosBridgeSandbox,
+  WebKitMessageHandler,
+} from './bridgeScript'
